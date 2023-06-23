c7f1ee46c38523610a299de547526afb9b9b509c
Ahmet Necdet Yalçın <ahmetnecdetyalcin@Ahmet-MacBook-Air.local>
2023-06-23 20:11:03 +0300

    dosya güncelledni

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,28 @@
 import React from "react";
-
+import { formatDistanceToNow, differenceInDays } from 'date-fns';
+import { tr } from 'date-fns/locale';
 const Task = ({ taskObj, onComplete }) => {
 
+
+  const daysRemaining = formatDistanceToNow(
+    new Date(taskObj.deadline),
+    {
+      addSuffix: true,
+      locale: tr
+    }
+
+  );
+  const isDeadlineClose = differenceInDays(
+    new Date(taskObj.deadline),
+    new Date()
+
+  )<=3;
+ const isDeadlineCloseClass = isDeadlineClose ? "bg-[#ffd9d4]": "bg-[#2cc521]"
+
   return (
-    <div className="task">
-      <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+    <div className="shadow-md bg-[#fff] mt-4 p-6 rounded leading-normal">
+      <h3 className="text-lg text-[#db874b]">{taskObj.title}</h3>
+      <div className="text-xs pt-1">son teslim: <span className={`p-1 rounded-sm ${isDeadlineCloseClass}`}>{daysRemaining}</span></div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
